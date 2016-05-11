89d38e5afdfa9258d2c75773de30029a9c560090
Derek Ogle <dogle@northland.edu>
2016-05-10 21:58:22 -0500

    Removed WP and put ts in

--- iCalc_tpr.R
+++ iCalc_tpr.R
@@ -1,12 +1,12 @@
 ################################################################################
 ## internal function to compute t-prime
 ################################################################################
-iCalc_tpr <- function(t,WP,NGT) {
+iCalc_tpr <- function(t,ts,NGT) {
   ## Step 1
-  SNG <- WP-NGT/2
+  SNG <- ts+(1-NGT)/2
   tmp.t <- t-SNG
   ## Step 2 (in parentheses) and Step 3
-  tmp.t2 <- (tmp.t-floor(tmp.t)) - NGT
+  tmp.t2 <- (tmp.t-floor(tmp.t))-NGT
   ## Step 4
   tmp.t2[tmp.t2<0] <- 0
   ## Step 5 (in parentheses) and Step 6 (also returns value)
